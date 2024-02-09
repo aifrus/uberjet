76347ebbff0d9e7478a4aa009a6bcd984975c5de
Russell Purinton <russell.purinton@gmail.com>
2024-02-09 13:48:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function initMap() {
                 stylers: [{ visibility: 'on' }]
             },
             {
-                featureType: 'parks',
+                featureType: 'poi',
                 stylers: [{ visibility: 'off' }]
             },
             {
