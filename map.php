9041bbf456233b64c752ac1e995eda381cba85bd
Russell Purinton <russell.purinton@gmail.com>
2024-02-09 13:19:26 -0500

    Update map.php

--- map.php
+++ map.php
@@ -12,19 +12,6 @@
 
 <body>
     <div id="map"></div>
-
-    <script>
-        function initMap() {
-            const center = {
-                lat: 28.4293889,
-                lng: -81.3090000
-            };
-            const map = new google.maps.Map(document.getElementById("map"), {
-                center: center,
-                zoom: 10, // Adjust the zoom level as needed
-            });
-        }
-    </script>
     <script src="js.php" async defer></script>
 </body>
 
