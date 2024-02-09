d08784b2c86661c6cdf3766112d77b11839cd342
Russell Purinton <russell.purinton@gmail.com>
2024-02-09 13:34:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                 }));
             d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))
         })({
-            key: "",
+            key: "AIzaSyC828mIxaroUaDQ2JmGvyOv8tjynHvSGXg",
             v: "weekly",
             // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
             // Add other bootstrap parameters as needed, using camel case.
